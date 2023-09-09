8b34f1cc6090b2f2735aac116a8078a8aa8914e2
PaulCohen <me@pjcohen.com>
2023-09-09 09:14:50 -0600

    else if statementes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
     function computerplay () {
-        let rock = "Rock";
-        let paper = "Paper";
-        let scissors = "Scissors";
+        const rock = "Rock";
+        const paper = "Paper";
+        const scissors = "Scissors";
         let getrandomvalue = math.random();
         //console.log(getrandomvalue);
         if (getrandomvalue <= 0.33) {
@@ -10,4 +10,12 @@
         } else if (getrandomvalue <= 0.66) {
             //console.log(paper);
             return paper;
-            
+        }else {
+            //console.log(siccors);
+            return scissors;
+        }
+    }
+
+    function playround(firstlettercap, computerselection) {
+        let tie = "It's a Tie! you selected " + firstlettercap + " and the computer selected " + computerselection + ".";
+        let paperBeatrock = "you Win! you selected " + firstlettercap + " and the computer selected "
