2acd0510702e7da311a705840473213f7d185349
PaulCohen <me@pjcohen.com>
2023-09-22 12:19:05 -0600

    disabled buttons

--- app.js
+++ app.js
@@ -32,6 +32,7 @@
             })
         })
 
+
         function playRound(playerSelection, computerSelection) {
             let tie = "It's a Tie! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
             let paperBeatRock = "You Win! You selected " + playerSelection + "and the computer selected " + computerSelection + ".";
@@ -40,6 +41,68 @@
             let rockBeatScissors = "You Win! You selected " + playerSelection + "and the computer selected " + computerSelection + ".";
             let rockBeatScissorsLoss = "You Win! You selected " + playerSelection + "and the computer selected " + computerSelection + ".";
             let scissorsBeatPaper = "You WiN! You selected " + playerSelection + "and the computer selected " + computerSelection + ".";
+ 
+
+            if (playerSelection === computerSelection){
+                return tie:
+            } else if ((playerSelection === "Paper") && (computerSelection === "Rock")) {
+               computerWin++;
+               return paperBeatRock; 
+            } else if ((playerSelection === "Paper") && (computerSelection === "Scissors")){
+            computerWin++;
+            return scissorsBeatPaperLoss;
+            } else if  ((playerSelection === "Rock") && ( computerSelection === "Paper")){
+                computerWin++;
+                return paperBeatRockLoss;
+            } else if  ((playerSelection === "Rock") && (computerSelection === "Scissors")){
+                playerWin++;
+                return rockBeatScissors;
+            } else if  ((playerSelection === "Scissors") && (computerSelection === "Rock")){
+                computerWin++;
+                return rockBeatScissorsLoss;
+            } else {
+                playerWin++;
+     }
+
+
+     // Creat a div Dom for all results
+      const container = document.querySelector("#container");
+      const resultsDiv = document.createElement("div");
+      resultsDiv.style.marginTop = "20px";
+      container.appendChild(resultsDiv);
+
+      // creat a player win trcking Dom
+      const playerWinText = document.createElement("p");
+      playerWinText.style.color = "blue";
+      computerWinText.textContent = "Computer Win totals: " + computerWin;
+      resultsDiv.appendChild(computerWinText);
+
+      // create computer win tracking Dom
+      const computerWinText = document.createElement("p");
+      computerWinText.style.color = "blue";
+      computerWinText.textContent = "Computer Win totals: " + computerWin;
+      resultsDiv.appendChild(computerWinText);
+
+      // creat  battle win text Dom 
+     const battleWinText = document.createElement("p");
+     battleWinText.style.color = "black";
+     resultsDiv.appendChild(battleWinText);
+
+     // creat game win text DOM
+     const gameWinText = document.createElement("p");
+     gameWinText.style.color = "orange";
+     gameWinText.textContent = gameWinner;
+     resultsDiv.appendChild(gameWinText);
+
+     // who won the five points first 
+     function endGame() {
+        if (playerWin == 5) {
+            gameWinText.textContent = gameWinner;
+
+
+            // disabled game buttons
             
         }
-    
+     }
+
+        }
