6882b57fe4517cc42d4f0238962b52b3de26ef5d
PaulCohen <me@pjcohen.com>
2023-09-23 12:00:29 -0600

    making buttons work

--- app.js
+++ app.js
@@ -7,10 +7,36 @@ function computerplay() {
     return choices[randomIdex];
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     if(playerSelection === computerSelection) {
         return {message: "It's a tie!", color: "black"};
-    } else if (
-        
-    )
+    } else if (   
+        (playerSelection === "Rock" && computerSelection === "Scissors")||
+        (playerSelection === "Paper" && computerSelection === "Rock")||
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        playWin++;
+        return {message: "You win this round!", color: "blue" };
+    } else {
+        computerWin++;
+        return { message: "You lose this round!", color: "red" };
+    }
+}
+
+function checkWinner() {
+    if (playWin == 5) {
+        return "Player, you won! Scum bag!";
+    }else if ( computerWin === 5 ) {
+        return "Computer wins! Better luck next time Dick face.";
+    }
+    return null;
+}
+
+document.addEventListener("DOMContentLoaded",() => {
+    const buttons = document.querySelectorAll("button");
+    const resulDiv = document.querySelector("#container");
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {}
+    }
 }
